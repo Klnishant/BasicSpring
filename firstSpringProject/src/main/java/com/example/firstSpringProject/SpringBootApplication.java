fab916685869e97f7172154b1ed05a368086228b
Klnishant <nishantkaushal075@gmail.com>
2024-04-23 22:57:03 +0530

    Basic setup for spring framework

---
+++ firstSpringProject/src/main/java/com/example/firstSpringProject/SpringBootApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package src.main.java.com.example.firstSpringProject;
+
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+@Documented
+@Retention(CLASS)
+/**
+ * 
+ */
+public @interface SpringBootApplication {
+
+}
